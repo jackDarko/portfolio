5f87f8341205d29955089840e653eb94219fbfe6
jackDarko <jackmichaela1@gmail.com>
2026-02-17 20:19:36 -0400

    Actualizo diseño del hero section

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Hero } from './hero/hero'
 import {Navbar} from './navbar/navbar'
+import { Start } from './start/start';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Hero, Navbar],
+  imports: [RouterOutlet, Hero, Navbar, Start],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
